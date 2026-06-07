61e6d153a37d2e485e028b54e93514441f74f971
Bhavneet Bhavneet <Bhavneet11d@gmail.com>
2026-06-07 15:13:28 -0400

    Implement AppointmentService for managing appointments

---
+++ Backend/src/main/java/com/fiveriverbarber/services/AppointmentService.java
@@ -0,0 +1,36 @@
+package com.fiveriverbarber.service;
+
+import com.fiveriverbarber.entity.Appointment;
+import com.fiveriverbarber.repository.AppointmentRepository;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AppointmentService {
+
+    private final AppointmentRepository repository;
+
+    public AppointmentService(
+            AppointmentRepository repository) {
+
+        this.repository = repository;
+    }
+
+    public Appointment saveAppointment(
+            Appointment appointment) {
+
+        return repository.save(appointment);
+    }
+
+    public List<Appointment> getAllAppointments() {
+
+        return repository.findAll();
+    }
+
+    public void deleteAppointment(Long id) {
+
+        repository.deleteById(id);
+    }
+}
